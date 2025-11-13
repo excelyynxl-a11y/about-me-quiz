b9413f2a563bacfbf7446fb1c774acf997660e6b
excel <elim0079@student.monash.edu>
2025-11-13 15:23:20 +0800

    create quiz and fucntions

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './Quiz.css';
 import { data } from '../../assets/data';
 
@@ -6,22 +6,97 @@ const Quiz = () => {
 
     let [index, setIndex] = useState(0);
     let [question, setQuestion] = useState(data[index]);
+    let [lock, setLock] = useState(false);
+    let [score, setScore] = useState(0);
+    let [result, setResult] = useState(false);
+
+    let option1 = useRef(null);
+    let option2 = useRef(null);
+    let option3 = useRef(null);
+    let option4 = useRef(null);
+
+    let optionArray = [option1, option2, option3, option4];
+
+    const checkAns = (element, answer) => {
+        if (lock === false){
+            if (question.answer === answer) {
+                element.target.classList.add("correct");
+                setLock(true);
+                setScore(prev => prev + 1);
+            } else {
+                element.target.classList.add("wrong");
+                setLock(true);
+                optionArray[question.answer - 1].current.classList.add("correct");
+            }
+        }
+    }
+
+    const next = () => {
+        if (lock === true){
+            if (index === data.length - 1) {
+                setResult(true);
+                return 0;
+            }
+            setIndex(++index);
+            setQuestion(data[index]);
+            setLock(false);
+            optionArray.map((option) => {
+                option.current.classList.remove("wrong");
+                option.current.classList.remove("correct");
+                return null;
+            })
+        }
+    }
+
+    const reset = () => {
+        setIndex(0);
+        setQuestion(data[0]);
+        setScore(0);
+        setLock(false);
+        setResult(false);
+    }
 
     return (
     <div className='container'>
         <h1>Quiz About Excelyynx</h1>
         <hr /> 
+        {result ? 
+        <></>
+        :
+        <>
         <h2> {index + 1}. {question.question}</h2>
         <ul>
-            <li>{question.option1}</li>
-            <li>{question.option2}</li>
-            <li>{question.option3}</li>
-            <li>{question.option4}</li>
+            <li ref={option1} onClick={(element) => {checkAns(element, 1)}}>
+                {question.option1}
+            </li>
+            <li ref={option2} onClick={(element) => {checkAns(element, 2)}}>
+                {question.option2}
+            </li>
+            <li ref={option3} onClick={(element) => {checkAns(element, 3)}}>
+                {question.option3}
+            </li>
+            <li ref={option4} onClick={(element) => {checkAns(element, 4)}}>
+                {question.option4}
+            </li>
         </ul>
-        <button>Next</button>
+        <button onClick={next}>Next</button>
         <div className='index'>
-            1 of 5 questions
+            {index + 1} of {data.length} questions
         </div>
+        </>
+        }
+
+        {result ? 
+        <>
+        <h2>
+            You Scored {score} out of {data.length}
+        </h2>
+        <button onClick={reset}>
+            Reset
+        </button>
+        </>
+        :
+        <></>}
     </div>
     )
 }

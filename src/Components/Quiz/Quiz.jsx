0cd06474a1304bcd05147a6cad157180b6ba461f
excel <elim0079@student.monash.edu>
2025-11-13 14:22:11 +0800

    first commit

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import './Quiz.css';
+import { data } from '../../assets/data';
+
+const Quiz = () => {
+
+    let [index, setIndex] = useState(0);
+    let [question, setQuestion] = useState(data[index]);
+
+    return (
+    <div className='container'>
+        <h1>Quiz About Excelyynx</h1>
+        <hr /> 
+        <h2> {index + 1}. {question.question}</h2>
+        <ul>
+            <li>{question.option1}</li>
+            <li>{question.option2}</li>
+            <li>{question.option3}</li>
+            <li>{question.option4}</li>
+        </ul>
+        <button>Next</button>
+        <div className='index'>
+            1 of 5 questions
+        </div>
+    </div>
+    )
+}
+
+export default Quiz
---
+++ src/assets/data.js
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        question: "What is Ex favourite thing?",
+        option1: "Cony bunny",
+        option2: "Cute sticker",
+        option3: "Bed and blanket",
+        option4: "Brush pens",
+        ans: 1,
+    },
+    {
+        question: "What is Ex favourite fruit?",
+        option1: "Guava",
+        option2: "Watermelon",
+        option3: "Pineapple",
+        option4: "Mango",
+        ans: 2,
+    },
+    {
+        question: "What is Ex MBTI?",
+        option1: "ENFP",
+        option2: "ISTP",
+        option3: "INTJ",
+        option4: "ESTJ",
+        ans: 3,
+    },
+    {
+        question: "Which is Ex favourite day of the week?",
+        option1: "Tuesday",
+        option2: "Thursday",
+        option3: "Friday",
+        option4: "Saturday",
+        ans: 4,
+    },
+]
